2b4b1f092bb9822ed9e1ee71c3e5fb4de00e39ac
han <chos9715@naver.com>
2022-09-11 22:49:27 +0900

    applicationContext

--- src/main/java/com/zb/convpay/config/ApplicationConfig.java
+++ src/main/java/com/zb/convpay/config/ApplicationConfig.java
@@ -1,26 +1,54 @@
 package com.zb.convpay.config;
 
 import com.zb.convpay.service.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
 import java.util.HashSet;
 
+@Configuration
 public class ApplicationConfig {
-    public ConveniencePayService conveniencePayServiceDiscountConvenience() {
+//    public ConveniencePayService conveniencePayServiceDiscountConvenience() {
+//        return new ConveniencePayService(
+//                new HashSet<>(
+//                        Arrays.asList(new MoneyAdapter(), new CardAdapter())
+//                ),
+//                new DiscountByConvenience()
+//        );
+//    }
+//
+//    public ConveniencePayService conveniencePayServiceDiscountPayMethod() {
+//        return new ConveniencePayService(
+//                new HashSet<>(
+//                        Arrays.asList(new MoneyAdapter(), new CardAdapter())
+//                ),
+//                new DiscountByPayMethod()
+//        );
+//    }
+
+    @Bean
+    public ConveniencePayService conveniencePayService(){
         return new ConveniencePayService(
                 new HashSet<>(
-                        Arrays.asList(new MoneyAdapter(), new CardAdapter())
+                        Arrays.asList(moneyAdapter(), cardAdapter())
                 ),
-                new DiscountByConvenience()
+                discountByConvenience()
         );
     }
 
-    public ConveniencePayService conveniencePayServiceDiscountPayMethod() {
-        return new ConveniencePayService(
-                new HashSet<>(
-                        Arrays.asList(new MoneyAdapter(), new CardAdapter())
-                ),
-                new DiscountByPayMethod()
-        );
+    @Bean
+    public CardAdapter cardAdapter() {
+        return new CardAdapter();
+    }
+
+    @Bean
+    public MoneyAdapter moneyAdapter() {
+        return new MoneyAdapter();
+    }
+
+    @Bean
+    public DiscountByConvenience discountByConvenience(){
+        return new DiscountByConvenience();
     }
 }
