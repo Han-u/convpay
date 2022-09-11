1f6fa6ae3f47e46900398df94f9873b9a6646762
han <chos9715@naver.com>
2022-09-12 00:05:40 +0900

    bean xml

--- src/main/java/com/zb/convpay/UserClient.java
+++ src/main/java/com/zb/convpay/UserClient.java
@@ -10,6 +10,7 @@ import com.zb.convpay.type.ConvenienceType;
 import com.zb.convpay.type.PayMethodType;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class UserClient {
     public static void main(String[] args) {
@@ -18,6 +19,7 @@ public class UserClient {
 //        ConveniencePayService conveniencePayService = applicationConfig.conveniencePayServiceDiscountPayMethod();
 
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+//         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
 
         ConveniencePayService conveniencePayService = applicationContext.getBean("conveniencePayService", ConveniencePayService.class);
 
